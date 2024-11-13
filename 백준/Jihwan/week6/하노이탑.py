d0e580dd2292b428535775955cf90ff342ddf9de
jihwan <ksw5464980@naver.com>
2024-11-13 21:02:17 +0900

    [Baekjoon-jh] week06

--- 백준/Jihwan/week6/하노이탑.py
+++ 백준/Jihwan/week6/하노이탑.py
@@ -6,9 +6,9 @@ def move(a, b, n):
         print(a, b) # 1~3을 움직인다고 하면 1을 움직일때
 
         return 0
-    move(a, 6-a-b, n-1)
+    move(a, 6-a-b, n-1) # (2, 1, 1) b=3, a= 2
     print(a, b) # 1~3을 움직인다고 하면 3을 움직일때
-    move(6-a-b, b, n-1)
+    move(6-a-b, b, n-1) # (2, 3, 2)
     return 0
 
-move(1, 3, n)
+move(1, 3, n) 
