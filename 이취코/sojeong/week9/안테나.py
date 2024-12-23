ffca2e57bd760447947ad02b4de372adbdadf0bf
Soojeoong <soojeoong43@gmail.com>
2024-12-23 21:39:33 +0900

    [이취코_sj] week09

---
+++ 이취코/sojeong/week9/안테나.py
@@ -0,0 +1,15 @@
+# 안테나는 집이 있는 곳에만 설치할 수 있기 때문에 중앙값 이용용
+
+N = int(input()) # 집의 수 N
+
+houses = list(map(int, input().split()))
+
+houses.sort()
+
+# 중앙값 O(1)
+result = houses[(N-1)//2]
+print(result)
+
+# # 평균값 -> 틀림림
+# result = sum(houses)//N
+# print(result)
